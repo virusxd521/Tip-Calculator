c5a52d182b3cd091032cabdd537226689208fec3
Daniel <virusxd521@gmail.com>
2021-07-17 13:58:54 +0300

    Adding final changes to the App

--- src/app.js
+++ src/app.js
@@ -4,17 +4,8 @@ const number_of_people = document.getElementById("people-input");
 const paragrapth_individual_tip = document.getElementById("tip-pay");
 const paragrapth_tip_plus_payment = document.getElementById("total-amoun-of-all");
 const erro_number_of_people_p = document.getElementById("cant-be-zero");
-
-
-// formula ---- 
-// percentage / 100 (15 / 100)
-// the result (in this example = 0.15)
-// multipy the result with the total bill 
-// in this example (0.15 * 142.55);
-//  the result os 21.3825
-// devide by the number of people
-// in this example 5 
-// 21.3825 / 5 = 4.27
+const customTipButton = document.getElementById("custom-tip");
+const end_bill_value_number = end_bill.value;
 
 
 function resultOfDevisionPerPerson(allThePeople, percentageOfTip, TotalBillToPay){
@@ -28,10 +19,13 @@ function resultOfDevisionPerPerson(allThePeople, percentageOfTip, TotalBillToPay
     const billsForEachPerson_with_tip = TotalBillToPay / allThePeople;
     const perPersonTip_plusPayment = tipPerPerson + billsForEachPerson_with_tip;
 
-    paragrapth_individual_tip.innerHTML = `$${tipPerPerson}`;
-    paragrapth_tip_plus_payment.innerHTML = `$${perPersonTip_plusPayment}`;
-    
+    paragrapth_individual_tip.innerHTML = `$${tipPerPerson.toFixed(2)}`;
+    paragrapth_tip_plus_payment.innerHTML = `$${perPersonTip_plusPayment.toFixed(2)}`;
+    console.log(22222222222222);
 }
+
+
+
 
 
 
@@ -40,15 +34,27 @@ function resultOfDevisionPerPerson(allThePeople, percentageOfTip, TotalBillToPay
 for(let i = 0; i < button_inputs.length; i++){
     console.log(button_inputs[i]);
     button_inputs[i].addEventListener("click", event => {
-        const end_bill_value_number = end_bill.value;
+        
         if( parseInt(number_of_people.value) > 0 ){
             
+            // Adding Custom tag to the input
+            // customTipButton.innerHTML = "Custom";
+            if (button_inputs[i].value.toUpperCase() === "CUSTOM"){
+                customTipButton.setAttribute("type", "number")
+                customTipButton.addEventListener("change", event => {
+                        console.log("changed")
+                        resultOfDevisionPerPerson(parseInt(number_of_people.value), 
+                        parseInt(customTipButton.value),
+                        parseFloat(end_bill_value_number))
+                    
+                    
+                    })
+            }
+
             resultOfDevisionPerPerson(parseInt(number_of_people.value), 
                             parseInt(button_inputs[i].value),
                             parseFloat(end_bill_value_number)
                             );
-
-      
         } else {
             erro_number_of_people_p.style.display = "inline-block";
             number_of_people.style.border = "solid 2px red"
