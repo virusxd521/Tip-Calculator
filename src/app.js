3ba605ad335846cff2672080f37dec1b55d7a841
Daniel <virusxd521@gmail.com>
2021-07-31 15:06:03 +0300

    fixed issues

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const number_of_people = document.getElementById("people-input");
 const paragrapth_individual_tip = document.getElementById("tip-pay");
 const paragrapth_tip_plus_payment = document.getElementById("total-amoun-of-all");
 const erro_number_of_people_p = document.getElementById("cant-be-zero");
+const error_no_bill = document.getElementById("cant-be-zero_number");
 const customTipButton = document.getElementById("custom-tip");
 
 
@@ -35,15 +36,15 @@ for(let i = 0; i < button_inputs.length; i++){
   
     button_inputs[i].addEventListener("click", event => {
         const end_bill_value_number = end_bill.value;
-        if( parseInt(number_of_people.value) > 0 ){
-            
+        if( parseInt(number_of_people.value) > 0 && end_bill_value_number !== "" &&
+            parseInt(end_bill_value_number) > 0){
+           
             // Adding Custom tag to the input
             // customTipButton.innerHTML = "Custom";
             if (button_inputs[i].value.toUpperCase() === "CUSTOM"){
                 customTipButton.setAttribute("type", "number");
 
-                customTipButton.addEventListener("change", event => {
-                  
+                customTipButton.addEventListener("change", event => {                
                     resultOfDevisionPerPerson(parseInt(number_of_people.value), 
                     parseInt(customTipButton.value),
                     parseFloat(end_bill_value_number))
@@ -57,11 +58,34 @@ for(let i = 0; i < button_inputs.length; i++){
                             parseInt(button_inputs[i].value),
                             parseFloat(end_bill_value_number)
                             );
-                            
+        
+            // display error for the number of people                
+            erro_number_of_people_p.style.display = "none";
+            number_of_people.style.border = "none";
+            // display error for the bill input
+            error_no_bill.style.display = "none";
+            end_bill.style.border = "none";
+            
+
+        } else if(parseInt(number_of_people.value) <= 0 || 
+        number_of_people.value === "" &&
+        parseInt(end_bill_value_number) <= 0 || end_bill_value_number === "") {
+            // display error for the number of people
+            erro_number_of_people_p.style.display = "inline-block";
+            number_of_people.style.border = "solid 2px red";
+            // display error for the bill input
+            error_no_bill.style.display = "inline-block";
+            end_bill.style.border = "solid 2px red";
+        } else if(parseInt(end_bill_value_number) <= 0 || end_bill_value_number === ""){
+         
+            error_no_bill.style.display = "inline-block";
+            end_bill.style.border = "solid 2px red";
         } else {
             erro_number_of_people_p.style.display = "inline-block";
-            number_of_people.style.border = "solid 2px red"
+            number_of_people.style.border = "solid 2px red";
         }
+
+
 
     })
     
